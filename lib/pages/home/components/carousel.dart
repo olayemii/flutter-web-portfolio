19fe4620b9bcb77350a3d4f5a47f40b9c625aeab
Olayemii <donyemisco@gmail.com>
2021-02-09 23:33:18 +0100

    build desktop screen

--- lib/pages/home/components/carousel.dart
+++ lib/pages/home/components/carousel.dart
@@ -8,6 +8,7 @@ import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:web_portfolio/pages/home/components/carousel_item.dart';
 import 'package:web_portfolio/pages/home/components/page_indicator.dart';
+import 'package:web_portfolio/utils/adaptive_layout.dart';
 import 'package:web_portfolio/utils/constants.dart';
 import 'package:web_portfolio/utils/mouse_region_span.dart';
 
@@ -28,98 +29,110 @@ class Carousel extends StatelessWidget {
         ),
       ),
       child: RepaintBoundary(
-        child: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: CarouselSlider(
-                      carouselController: controller,
-                      options: CarouselOptions(
-                        viewportFraction: 1.0,
-                        scrollPhysics: NeverScrollableScrollPhysics(),
-                      ),
-                      items: [1, 2, 3, 4, 5].map((i) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            return CarouselItem();
-                          },
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  PageIndicator(currentIndex: currentIndex),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                left: 50.0,
-              ),
-              child: ValueListenableBuilder(
-                  valueListenable: currentIndex,
-                  builder: (context, value, child) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          child: GestureDetector(
-                            onTap: () {
-                              if (value > 0) {
-                                controller.animateToPage(
-                                  value - 1,
-                                  duration: Duration(
-                                    milliseconds: 450,
-                                  ),
-                                  curve: Curves.easeIn,
-                                );
-                                currentIndex.value = value - 1;
-                              }
-                            },
-                            child: Icon(
-                              FlutterIcons.caret_square_left_faw5,
-                              color: kPrimaryColor,
-                              size: 28.0,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 15.0,
-                        ),
-                        MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          child: GestureDetector(
-                            onTap: () {
-                              if (value < 4) {
-                                controller.animateToPage(
-                                  value + 1,
-                                  duration: Duration(
-                                    milliseconds: 450,
-                                  ),
-                                  curve: Curves.easeIn,
-                                );
-                                currentIndex.value = value + 1;
-                              }
-                            },
-                            child: Icon(
-                              FlutterIcons.caret_square_right_faw5,
-                              color: kPrimaryColor,
-                              size: 28.0,
-                            ),
-                          ),
-                        )
-                      ],
-                    );
-                  }),
-            ),
-          ],
+        child: AdaptiveLayout(
+          desktop: buildDesktopScreen(),
+          mobile: buildDesktopScreen(),
+          tablet: buildDesktopScreen(),
         ),
       ),
+    );
+  }
+
+  Widget slideChanger() {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 50.0,
+      ),
+      child: ValueListenableBuilder(
+          valueListenable: currentIndex,
+          builder: (context, value, child) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (value > 0) {
+                        controller.animateToPage(
+                          value - 1,
+                          duration: Duration(
+                            milliseconds: 450,
+                          ),
+                          curve: Curves.easeIn,
+                        );
+                        currentIndex.value = value - 1;
+                      }
+                    },
+                    child: Icon(
+                      FlutterIcons.caret_square_left_faw5,
+                      color: kPrimaryColor,
+                      size: 28.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (value < 4) {
+                        controller.animateToPage(
+                          value + 1,
+                          duration: Duration(
+                            milliseconds: 450,
+                          ),
+                          curve: Curves.easeIn,
+                        );
+                        currentIndex.value = value + 1;
+                      }
+                    },
+                    child: Icon(
+                      FlutterIcons.caret_square_right_faw5,
+                      color: kPrimaryColor,
+                      size: 28.0,
+                    ),
+                  ),
+                )
+              ],
+            );
+          }),
+    );
+  }
+
+  Widget buildDesktopScreen() {
+    return Column(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: CarouselSlider(
+                  carouselController: controller,
+                  options: CarouselOptions(
+                    viewportFraction: 1.0,
+                    scrollPhysics: NeverScrollableScrollPhysics(),
+                  ),
+                  items: [1, 2, 3, 4, 5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return CarouselItem();
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              PageIndicator(currentIndex: currentIndex),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 40.0,
+        ),
+        slideChanger(),
+      ],
     );
   }
 }
