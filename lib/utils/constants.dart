d0844fbf9b4db2fdcc78448c2a9d4298ebd2f136
Olayemii <donyemisco@gmail.com>
2021-02-09 14:01:34 +0100

    begin theme setup

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color.fromRGBO(21, 181, 114, 1);
+const Color kBackgroundColor = Color.fromRGBO(7, 17, 26, 1);
+const Color kDangerColor = Color.fromRGBO(249, 77, 30, 1);
